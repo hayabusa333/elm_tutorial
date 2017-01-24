d3153e66695f482a079363eee577a482b074cf42
hayabusa333 <junnichi0121@gmail.com>
2017-01-25 08:36:34 +0900

    Payload Massage

---
+++ Main.elm
@@ -0,0 +1,52 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+-- モデル
+
+type alias Model =
+  Int
+
+init : ( Model, Cmd Msg )
+init =
+  ( 0, Cmd.none )
+
+-- メッセージ
+
+type Msg
+  = Increment Int
+
+-- ビュー
+
+view : Model -> Html Msg
+view model =
+  div []
+      [ button [ onClick (Increment 2) ] [ text "+" ]
+      , text (toString model)
+      ]
+
+-- 更新
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Increment howMuch ->
+      ( model + howMuch, Cmd.none )
+
+-- サブスクリプション
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- MAIN
+
+main : Program Never Model Msg
+main =
+  program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
