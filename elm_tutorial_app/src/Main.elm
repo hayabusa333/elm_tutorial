83c4a79d2978491f58c8a384def871f1e1ea88fb
hayabusa333 <junnichi0121@gmail.com>
2017-01-25 13:06:05 +0900

    Multiple modules

--- elm_tutorial_app/src/Main.elm
+++ elm_tutorial_app/src/Main.elm
@@ -1,60 +1,20 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
-
--- モデル
-
-
-type alias Model =
-    String
-
+import Messages exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
 init : ( Model, Cmd Msg )
 init =
     ( "Hello", Cmd.none )
 
-
-
--- メッセージ
-
-
-type Msg
-    = NoOp
-
-
-
--- ビュー
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- 更新
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- サブスクリプション(購読)
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
-
-
 -- MAIN
-
 
 main : Program Never Model Msg
 main =
@@ -64,3 +24,4 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
+
